84302813c5ce5b5aa830cd08d022a1efcbeff1eb
Fedor Bokovikov <bokoviko@amazon.com>
2021-02-09 20:44:00 -0800

    Remove unused imports

--- spring-hibernate/src/main/java/com/product/star/HibernateConfiguration.java
+++ spring-hibernate/src/main/java/com/product/star/HibernateConfiguration.java
@@ -3,10 +3,7 @@ package com.product.star;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Properties;
 
 @org.springframework.context.annotation.Configuration
 public class HibernateConfiguration {
